e249abe72636ef1f9edfb16bd647b48ed8a01d39
vincent <vincent.arteaga@gmail.com>
2021-08-03 21:16:24 -0700

    commit tree d283993b0a167f704d633ef8a79820e2a05b2406

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+    # macro that writes other code for us attribute accessor symbol 
+    attr_accessor :name
+
+    # init a object, defines default behavior 
+    # instance method
+    # def initialize(name)
+    #     @name = name
+    # end
+    # reader / getter
+    # def name 
+    #     @name
+    # end
+
+    # #writer / setter
+    # def name=(name)
+    #     @name = name
+    # end
+    
+end
+
+# artist_object = Artist.new("The Beatles")
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Artist" do 
   it "has a name" do
     artist = Artist.new
-    artist.name = "Beyonce" 
+    artist.name = "Beyonce"
     expect(artist.name).to eq("Beyonce")
   end
 end
